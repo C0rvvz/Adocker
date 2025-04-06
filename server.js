93d85dd6a3f8cb3d7fe60d32a762d66c7c0296c9
yeisont08 <yeisont08@gmail.com>
2025-04-05 19:27:24 -0500

    Primer cambio git, para reto 3

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require("http"); 
 const port = 3000; 
 const server = http.createServer((req, res) => { 
-res.writeHead(200, { "Content-Type": "text/plain" }); 
-res.end("Hello, Docker!"); 
+res.writeHead(200, { "Content-Type": "text/plain" });
+res.end("Hello, Docker!, este es el primer cambio con Git");
+res.end("otro mensaje");
 }); 
 server.listen(port, () => { 
 console.log(`Server running at http://localhost:${port}`); 
